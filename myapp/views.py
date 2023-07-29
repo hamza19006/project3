4c2bc673736c0255a71e9fa59cf77843146b66b7
hamza19006 <140162627+hamza19006@users.noreply.github.com>
2023-07-29 13:59:03 +0200

    Initial commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import book
 
 # Create your views here.
 def hello(request) :
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        date = request.POST['date']
+        Book = book(title = title , author = author , date = date)
+        Book.save()
+        return redirect('/')
+    else:
+        return render(request, 'index.html')
